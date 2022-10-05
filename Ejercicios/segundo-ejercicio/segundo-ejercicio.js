fcd46c60de455d9ed87c0d3ad32c76035f7ff9b4
EmmanuelCD <72762073+EmmanuelCD@users.noreply.github.com>
2022-10-04 22:50:28 -0400

    Segundo Ejercicio de JavaScript.
    Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
    - Tu nombre (string)
    - Tu edad (number)
    - ¿Eres desarrollador? (boolean)
    - Tu fecha de nacimiento (Date)
    - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)

---
+++ Ejercicios/segundo-ejercicio/segundo-ejercicio.js
@@ -0,0 +1,11 @@
+let lista =["Emmanuel",25,true,fecha = new Date(1997,04,18),
+  libro =
+{
+    titulo: "Looking For Alaska",
+    autor: "John Green",
+    fecha: new Date(2005,03,05),
+    url: "https://www.amazon.com/-/es/John-Green/dp/0525475060/ref=sr_1_1?crid=1EWM3R6EF0P18&keywords=looking+for+alaska+john+green&qid=1664937649&qu=eyJxc2MiOiIyLjIyIiwicXNhIjoiMS41NyIsInFzcCI6IjEuNTUifQ%3D%3D&sprefix=looking+fo%2Caps%2C157&sr=8-1",
+}
+];
+
+console.log(lista);
