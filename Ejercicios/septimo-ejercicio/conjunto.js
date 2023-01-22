0ac0f2643e1e15f9b248ca988d2010c0f37f0b03
EmmanuelCD <72762073+EmmanuelCD@users.noreply.github.com>
2023-01-22 17:57:13 -0400

    Create conjunto.js
    //Crea un archivo llamado conjuntos.js que contenga las siguientes líneas
    //- Un nuevo Set con los nombres de tu familia
    //- Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
    //- Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)

---
+++ Ejercicios/septimo-ejercicio/conjunto.js
@@ -0,0 +1,13 @@
+
+
+const family = ['Emmanuel', 'Odalis', 'Roselio', 'Rosely'];
+
+const conjunto = new Set(family);
+console.log(conjunto);
+
+conjunto.add('Emmanuel');
+
+console.log(conjunto);
+
+conjunto.add('Javascript');
+console.log(conjunto);
